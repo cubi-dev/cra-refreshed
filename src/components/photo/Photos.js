5f00612eb7cd208a588f8bcc0ee4e8985a0e9403
cubi-dev <cubiiwork@gmail.com>
2024-04-24 01:02:08 +0700

    Update useEffect refreshed without dependencies

---
+++ src/components/photo/Photos.js
@@ -0,0 +1,71 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const getRandomPhotos = (page) => {
+  return axios
+    .get(`https://picsum.photos/v2/list?page=${page}&limit=8`)
+    .then((response) => {
+      console.log(response.data);
+      return response.data;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+// https://picsum.photos/v2/list
+// https://picsum.photos/v2/list?page=2&limit=100
+
+const Photos = () => {
+  // useEffect(callback, [dependencies])
+  // useEffect(function callback() {
+  //     // side-effects
+  // }, []);
+  const [randomPhotos, setRandomPhotos] = useState([]);
+  const [nextPage, setNextPage] = useState(1);
+  //   console.log("outside useEffect");
+  const handleLoadMorePhotos = () => {
+    console.log(nextPage);
+    getRandomPhotos(nextPage).then((images) => {
+      console.log(images);
+      const newPhotos = [...randomPhotos, ...images];
+      setRandomPhotos(newPhotos);
+      setNextPage(nextPage + 1);
+    });
+  };
+  useEffect(() => {
+    // side-effects
+    // document.title = "Welcome to useEffect";
+    // console.log("inside useEffect");
+    handleLoadMorePhotos();
+  }, []);
+  return (
+    <div>
+      <div className="grid grid-cols-4 gap-5 p-5">
+        {randomPhotos.length > 0 &&
+          randomPhotos.map((item, index) => (
+            <div
+              key={item.id}
+              className="p-3 bg-white shadow-md rounded-lg h-[200px]"
+            >
+              <img
+                className="w-full h-full object-cover rounded-lg"
+                src={item.download_url}
+                alt={item.author}
+              />
+            </div>
+          ))}
+      </div>
+      <div className="text-center">
+        <button
+          onClick={handleLoadMorePhotos}
+          className="inline-block px-8 py-4 bg-purple-600 text-white"
+        >
+          Load more
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Photos;
