3e62a0ebaebc42b7104435d010e6e6b6686e5c28
cubi-dev <cubiiwork@gmail.com>
2024-04-25 16:40:34 +0700

    Example when using dependencies with object

--- src/components/counter/Counter.js
+++ src/components/counter/Counter.js
@@ -7,21 +7,27 @@ const Counter = () => {
   //         setCount((count) => count + 1);
   //     }, 1000);
   // };
-    const [info, setInfo] = useState({
-        firstName: "Nguyen", 
-        lastName: "Dung"
-    })
+  const [info, setInfo] = useState({
+    firstName: "Nguyen",
+    lastName: "Dung",
+  });
 
   useEffect(() => {
     console.log(`From input`);
-  }, [info]);
-  
+  }, [info.lastName]);
+
   return (
     <div className="p-5 flex gap-x-4 items-center">
-      <span className="text-2xl font-bold ">{count}</span>
+      <input
+        type="text"
+        name="lastName"
+        value={info.lastName}
+        onChange={(e) => setInfo({ ...info, lastName: e.target.value })}
+      />
+      {/* <span className="text-2xl font-bold ">{count}</span>
       <button onClick={() => setCount(count + 1)} className="inline-block p-3 bg-green-400 text-white">
         Increment
-      </button>
+      </button> */}
     </div>
   );
 };
