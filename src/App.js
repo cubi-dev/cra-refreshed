51d0c65f607c6fd6ea979f387a5d64715a0d7665
cubi-dev <cubiiwork@gmail.com>
2024-04-28 00:24:31 +0700

    Example useRef for timer

--- src/App.js
+++ src/App.js
@@ -3,41 +3,18 @@ import React from "react";
 import "./index.css";
 
 function App() {
-  // React.useEffect(() => {
-  //   const timer = setInterval(() => {
-
-  //   }, interval);
-  //   return () => clearInterval(timer);
-  // }, [])
-  const timerRef = React.useRef(null);
-  const [count, setCount] = React.useState(0);
-
-  const handleStart = () => {
-    if (timerRef.current) {
-      return;
-    }
-    timerRef.current = setInterval(() => {
-      setCount((prevCount) => prevCount + 1);
-    }, 1000);
+  // const [count, setCount] = React.useState(0);
+  const countRef = React.useRef(0);
+  const handle = () => {
+    const updateCount = countRef.current + 1;
+    console.log(`Clicked ${updateCount} times`);
+    // setCount(updateCount);
+    countRef.current++;
   };
-  const handleStop = () => {
-    clearInterval(timerRef.current);
-    timerRef.current = null;
-  };
-
-  React.useEffect(() => {
-    return () => {
-      clearInterval(timerRef.current);
-    };
-  }, []);
-
+  console.log("Render");
   return (
     <div>
-      <h3>Timer: {count}s</h3>
-      <div>
-        <button onClick={handleStart}>Start</button>
-        <button onClick={handleStop}>Stop</button>
-      </div>
+      <button onClick={handle}>Click me</button>
     </div>
   );
 }
