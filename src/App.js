e9d03b3bd221b05e751c1b9d77468d71f452f0d9
cubi-dev <cubiiwork@gmail.com>
2024-04-28 00:07:16 +0700

    Example of difference between useState & useRef

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import React from "react";
 import "./index.css";
 
 function App() {
-  // const inputRef, abcRef = React.useRef(initialValue);
+  // const [count, setCount] = React.useState(0);
   const countRef = React.useRef(0);
-  // truy xuất giá trị của ref 
-  console.log(countRef.current); // 0
+  const handle = () => {
+    const updateCount = countRef.current + 1;
+    console.log(`Clicked ${updateCount} times`);
+    // setCount(updateCount);
+    countRef.current++;
+  };
+  console.log("Render");
   return (
     <div>
-      
+      <button onClick={handle}>Click me</button>
     </div>
   );
 }
