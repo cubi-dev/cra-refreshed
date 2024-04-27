72116b738ed9c6a90e262d8724d8acabaa478caa
cubi-dev <cubiiwork@gmail.com>
2024-04-28 00:25:47 +0700

    Fix last commit change : reverse hunk commit

--- src/App.js
+++ src/App.js
@@ -3,18 +3,41 @@ import React from "react";
 import "./index.css";
 
 function App() {
-  // const [count, setCount] = React.useState(0);
-  const countRef = React.useRef(0);
-  const handle = () => {
-    const updateCount = countRef.current + 1;
-    console.log(`Clicked ${updateCount} times`);
-    // setCount(updateCount);
-    countRef.current++;
+  // React.useEffect(() => {
+  //   const timer = setInterval(() => {
+
+  //   }, interval);
+  //   return () => clearInterval(timer);
+  // }, [])
+  const timerRef = React.useRef(null);
+  const [count, setCount] = React.useState(0);
+
+  const handleStart = () => {
+    if (timerRef.current) {
+      return;
+    }
+    timerRef.current = setInterval(() => {
+      setCount((prevCount) => prevCount + 1);
+    }, 1000);
   };
-  console.log("Render");
+  const handleStop = () => {
+    clearInterval(timerRef.current);
+    timerRef.current = null;
+  };
+
+  React.useEffect(() => {
+    return () => {
+      clearInterval(timerRef.current);
+    };
+  }, []);
+
   return (
     <div>
-      <button onClick={handle}>Click me</button>
+      <h3>Timer: {count}s</h3>
+      <div>
+        <button onClick={handleStart}>Start</button>
+        <button onClick={handleStop}>Stop</button>
+      </div>
     </div>
   );
 }
