be48b5d6d275a3174a807cbdf4d31aa8ec619bc4
cubi-dev <cubiiwork@gmail.com>
2024-05-01 22:16:44 +0700

    Update: SignUpFormHook with Controller

--- src/components/form/SignUpFormHook.js
+++ src/components/form/SignUpFormHook.js
@@ -1,6 +1,6 @@
 //using react-hook-form
-import React from "react";
-import { useForm } from "react-hook-form";
+import React, { useEffect } from "react";
+import { Controller, useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import axios from "axios";
 import * as yup from "yup";
@@ -17,19 +17,25 @@ const SignUpFormHook = () => {
     register,
     handleSubmit,
     formState: { errors, isSubmitting, isValid, isDirty, dirtyFields },
+    watch,
+    reset,
+    setFocus,
+    setValue,
+    control,
   } = useForm({
     resolver: yupResolver(schemaValidation),
-    mode: "onChange", // khi sử dụng mode: "onChange" + check isValid thì nó sẽ check xem form có hợp lệ không mỗi khi mình chạm vô input
+    mode: "onChange",
   });
-  console.log("isValid", isValid); // Cái này là để kiểm tra xem form có hợp lệ không
-  console.log("isDirty", isDirty); // Cái này là để kiểm tra xem form có chạm vô cái input nào không
-  console.log("dirtyFields", dirtyFields); // Cái này tương tự như isDirty nhưng nó sẽ trả về object những cái input mà mình đã chạm vô
-  // errors = formState.errors
-  // console.log(errors);
-  // console.log(isSubmitting);
+  // console.log("isValid", isValid);
+  // console.log("isDirty", isDirty);
+  // console.log("dirtyFields", dirtyFields);
+  const watchShowAge = watch("showAge", false);
+  // console.log("watchShowAge", watchShowAge);
   const onSubmit = async (values) => {
     if (isValid) {
       console.log("Send data to server: ", values);
+      //After submit, reset form
+      reset({ firstName: "", lastName: "", email: "" });
     }
     // const response = await axios.get(
     //   "https://hn.algolia.com/api/v1/search?query=react"
@@ -42,6 +48,16 @@ const SignUpFormHook = () => {
     //   }, 5000);
     // });
   };
+  useEffect(() => {
+    setFocus("firstName");
+  }, [setFocus]);
+
+  const handleSetDemoData = () => {
+    setValue("firstName", "Cubidev");
+    setValue("lastName", "dung");
+    setValue("email", "abc@gmail.com");
+  };
+
   return (
     <form
       onSubmit={handleSubmit(onSubmit)}
@@ -67,27 +83,40 @@ const SignUpFormHook = () => {
         {/* {errors?.firstName?.type === "max" && (
           <div className="text-red-500 text-sm">{errors.firstName.message}</div>
         )} */}
-        <div className="flex flex-col gap-2 mb-5">
-          <label htmlFor="lastName">Lastname</label>
-          <input
-            type="text"
-            id="lastName"
-            placeholder="Enter your last name"
-            className="p-4 rounded-md border border-gray-100 focus:border-[rgba(41, 121, 255, 1)]"
-            {...register("lastName")}
-          />
-        </div>
+      </div>
+      <div className="flex flex-col gap-2 mb-5">
+        <label htmlFor="lastName">Lastname</label>
+        <input
+          type="text"
+          id="lastName"
+          placeholder="Enter your last name"
+          className="p-4 rounded-md border border-gray-100 focus:border-[rgba(41, 121, 255, 1)]"
+          {...register("lastName")}
+        />
+      </div>
 
-        <div className="flex flex-col gap-2 mb-5">
-          <label htmlFor="email">Email Address</label>
-          <input
-            type="email"
-            id="email"
-            placeholder="Enter your email address"
-            className="p-4 rounded-md border border-gray-100 focus:border-[rgba(41, 121, 255, 1)]"
-            {...register("email")}
-          />
-        </div>
+      <div className="flex flex-col gap-2 mb-5">
+        <label htmlFor="email">Email Address</label>
+        <MyInput
+          name="email"
+          placeholder="Enter your email address"
+          id="email"
+          control={control}
+        ></MyInput>
+        {/* <input
+          type="email"
+          id="email"
+          placeholder="Enter your email address"
+          className="p-4 rounded-md border border-gray-100 focus:border-[rgba(41, 121, 255, 1)]"
+          {...register("email")}
+        /> */}
+      </div>
+
+      <div className="flex flex-col gap-2 mb-5">
+        <input type="checkbox" {...register("showAge")} />
+        {watchShowAge && (
+          <input type="number" placeholder="Please enter your age" />
+        )}
       </div>
       <div>
         <button
@@ -101,7 +130,32 @@ const SignUpFormHook = () => {
           )}
         </button>
       </div>
+      <div>
+        <button
+          className="p-3 bg-green-500 text-wrap"
+          onClick={handleSetDemoData}
+        >
+          Demo data
+        </button>
+      </div>
     </form>
+  );
+};
+
+const MyInput = ({ control, ...props }) => {
+  return (
+    <Controller
+      name={props.name}
+      control={control}
+      defaultValue=""
+      render={({ field }) => (
+        <input
+          className="p-4 rounded-md border border-gray-100 focus:border-[rgba(41, 121, 255, 1)]"
+          {...field}
+          {...props}
+        />
+      )}
+    ></Controller>
   );
 };
 
